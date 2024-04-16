fc81a2e33a570279905cddc2dff8b648abbf29c3
Voicu Librimir <voicu.librimir@gmail.com>
2024-04-16 21:31:58 +0300

    Add project files.

---
+++ Verificarea unui număr prim.cpp
@@ -0,0 +1,38 @@
+﻿// Verificarea unui număr prim.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+bool estePrim(int numar) {
+    if (numar <= 1) {
+        return false;
+    }
+
+    int divisor = 2;
+    while (divisor * divisor <= numar) {
+        if (numar % divisor == 0) {
+            return false;
+        }
+        divisor++;
+    }
+
+    return true;
+}
+
+int main() {
+    int numar;
+
+    // Citim numărul de la utilizator
+    std::cout << "Introduceti un numar: ";
+    std::cin >> numar;
+
+    // Verificăm dacă numărul este prim
+    if (estePrim(numar)) {
+        std::cout << numar << " este un numar prim." << std::endl;
+    }
+    else {
+        std::cout << numar << " nu este un numar prim." << std::endl;
+    }
+
+    return 0;
+}
